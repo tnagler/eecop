089078b45c5ee276d760ebfdd6dcaec9161bbeaf
Thibault Vatter <thibault.vatter@gmail.com>
2019-08-27 16:19:39 -0400

    Renaming for consistency

--- R/copula_methods.R
+++ R/copula_methods.R
@@ -1,93 +1,97 @@
 #' @importFrom stats qnorm
 #' @noRd
-fit_copula_gaussian <- function(u, weights) {
-    rho <- wdm::wdm(qnorm(u), weights = weights)
-    cop_model <- copula::normalCopula(param = copula::P2p(rho),
-                                      dim = ncol(u),
-                                      dispstr = "un")
-    function(u_new) copula::dCopula(u_new, cop_model)
+fit_copula_normal <- function(u, weights) {
+  rho <- wdm::wdm(qnorm(u), weights = weights)
+  cop_model <- copula::normalCopula(
+    param = copula::P2p(rho),
+    dim = ncol(u),
+    dispstr = "un"
+  )
+  function(u_new) copula::dCopula(u_new, cop_model)
 }
 
 #' @noRd
 fit_copula_vine <- function(u, weights, ...) {
-    cop_model <- rvinecopulib::vinecop(u, weights = weights, ...)
-    function(u_new) rvinecopulib::dvinecop(u_new, cop_model)
+  cop_model <- rvinecopulib::vinecop(u, weights = weights, ...)
+  function(u_new) rvinecopulib::dvinecop(u_new, cop_model)
 }
 
 #' @importFrom stats qnorm dnorm
 #' @noRd
 fit_copula_kde <- function(u, weights) {
-    x <- qnorm(u)
-    bw <- nrow(x)^(-2 / (ncol(x) + 2)) * cov(x)
-    function(u_new) {
-        x_new <- qnorm(u_new)
-        n <- nrow(x)
-        if (length(weights) == 0) {
-            weights <- rep(1, n)
-        }
-        bws <- replicate(n, bw, simplify = FALSE)
-        bws <- do.call(rbind, bws)
-        f <- ks::dmvnorm.mixt(
-            x = x_new,
-            mus = x,
-            Sigmas = bws,
-            props = weights / sum(weights)
-        )
-        f / apply(dnorm(x_new), 1, prod)
+  x <- qnorm(u)
+  bw <- nrow(x)^(-2 / (ncol(x) + 2)) * cov(x)
+  function(u_new) {
+    x_new <- qnorm(u_new)
+    n <- nrow(x)
+    if (length(weights) == 0) {
+      weights <- rep(1, n)
     }
+    bws <- replicate(n, bw, simplify = FALSE)
+    bws <- do.call(rbind, bws)
+    f <- ks::dmvnorm.mixt(
+      x = x_new,
+      mus = x,
+      Sigmas = bws,
+      props = weights / sum(weights)
+    )
+    f / apply(dnorm(x_new), 1, prod)
+  }
 }
 
 #' @noRd
 fit_copula_bernstein <- function(u, weights) {
-    m <- ceiling(nrow(u)^(2 / (4 + ncol(u))))
-    fit <- fit_bern_coefs(u, m)
-    function(u_new) eval_bern(u_new, fit)
+  m <- ceiling(nrow(u)^(2 / (4 + ncol(u))))
+  fit <- fit_bern_coefs(u, m)
+  function(u_new) eval_bern(u_new, fit)
 }
 
 fit_bern_coefs <- function(u, m, weights = numeric(0)) {
-    if (!length(weights))
-        weights <- rep(1, NROW(u))
-    d <- NCOL(u)
+  if (!length(weights)) {
+    weights <- rep(1, NROW(u))
+  }
+  d <- NCOL(u)
 
-    ## compute (weighted) cell frequencies ----------
-    # compute cell indicators for each margin
-    ucut <- lapply(seq_len(d), function(j) cut(u[, j], seq(0, 1, len = m + 1)))
+  ## compute (weighted) cell frequencies ----------
+  # compute cell indicators for each margin
+  ucut <- lapply(seq_len(d), function(j) cut(u[, j], seq(0, 1, len = m + 1)))
 
-    # join indicators across margins
-    joined <- do.call(paste, ucut)
+  # join indicators across margins
+  joined <- do.call(paste, ucut)
 
-    # generate table of all possible joined cells
-    lvls <- do.call(expand.grid, lapply(ucut, levels))
-    lvls <- apply(lvls, 1, paste, collapse = " ")
+  # generate table of all possible joined cells
+  lvls <- do.call(expand.grid, lapply(ucut, levels))
+  lvls <- apply(lvls, 1, paste, collapse = " ")
 
-    # compute weighted frequency counts
-    joined <- factor(joined, levels = lvls)
-    coef <- tapply(weights, joined, sum) / NROW(u)
+  # compute weighted frequency counts
+  joined <- factor(joined, levels = lvls)
+  coef <- tapply(weights, joined, sum) / NROW(u)
 
-    # which set of basis functions corresponds to each cell
-    basis_ind <- expand.grid(replicate(d, seq_len(m + 1), simplify = FALSE)) - 1
+  # which set of basis functions corresponds to each cell
+  basis_ind <- expand.grid(replicate(d, seq_len(m + 1), simplify = FALSE)) - 1
 
-    # only store non-zero coefficients and basis functions
-    ind <- which(coef > 0)
-    list(basis_ind = as.matrix(basis_ind[ind, ]),
-         coef = coef[ind],
-         m = m,
-         d = d)
+  # only store non-zero coefficients and basis functions
+  ind <- which(coef > 0)
+  list(
+    basis_ind = as.matrix(basis_ind[ind, ]),
+    coef = coef[ind],
+    m = m,
+    d = d
+  )
 }
 
 bern_poly <- function(x, k, m) {
-    (choose(m, k) * x^k * (1 - x)^(m - k)) * (m + 1)
+  (choose(m, k) * x^k * (1 - x)^(m - k)) * (m + 1)
 }
 
 eval_bern <- function(u, fit) {
-    c <- 0
-    for (k in seq_len(nrow(fit$basis_ind))) {
-        p <- fit$coef[k]
-        for (j in seq_len(fit$d)) {
-            p <- p * bern_poly(u[, j], fit$basis_ind[k, j], fit$m)
-        }
-        c <- c + p
+  c <- 0
+  for (k in seq_len(nrow(fit$basis_ind))) {
+    p <- fit$coef[k]
+    for (j in seq_len(fit$d)) {
+      p <- p * bern_poly(u[, j], fit$basis_ind[k, j], fit$m)
     }
-    c
+    c <- c + p
+  }
+  c
 }
-
--- R/eecop.R
+++ R/eecop.R
@@ -14,81 +14,84 @@
 #' @examples
 eecop <- function(y, x, copula_method = "vine", margin_method = "kde",
                   weights = numeric(), ...) {
-    y <- as.matrix(y)
-    x <- as.matrix(x)
-    assert_that(
-        nrow(y) == nrow(x),
-        is.string(copula_method),
-        is.string(margin_method),
-        copula_method %in% c("vine", "gaussian", "kde", "bernstein"),
-        margin_method %in% c("kde", "normal"),
-        is.numeric(weights)
-    )
+  y <- as.matrix(y)
+  x <- as.matrix(x)
+  assert_that(
+    nrow(y) == nrow(x),
+    is.string(copula_method),
+    is.string(margin_method),
+    copula_method %in% c("vine", "normal", "kde", "bernstein"),
+    margin_method %in% c("kde", "normal"),
+    is.numeric(weights)
+  )
 
-    n <- nrow(y)
-    q <- ncol(y)
-    p <- ncol(x)
+  n <- nrow(y)
+  q <- ncol(y)
+  p <- ncol(x)
 
-    margins_Y <- lapply(
-        seq_len(q),
-        function(j) fit_margin(y[, j], margin_method, weights)
-    )
-    margins_X <- lapply(
-        seq_len(p),
-        function(j) fit_margin(x[, j], margin_method, weights)
-    )
+  margins_Y <- lapply(
+    seq_len(q),
+    function(j) fit_margin(y[, j], margin_method, weights)
+  )
+  margins_X <- lapply(
+    seq_len(p),
+    function(j) fit_margin(x[, j], margin_method, weights)
+  )
 
-    V <- sapply(seq_len(q), function(j) margins_Y[[j]](y[, j]))
-    U <- sapply(seq_len(p), function(j) margins_X[[j]](x[, j]))
-    c_YX <- fit_copula(cbind(V, U), copula_method, weights, ...)
-    c_Y <- fit_copula(V, copula_method, weights, ...)
+  V <- sapply(seq_len(q), function(j) margins_Y[[j]](y[, j]))
+  U <- sapply(seq_len(p), function(j) margins_X[[j]](x[, j]))
+  c_YX <- fit_copula(cbind(V, U), copula_method, weights, ...)
+  c_Y <- fit_copula(V, copula_method, weights, ...)
 
-    if (length(weights) == 0) {
-        weights <- rep(1, n)
-    }
-    weights <- weights / mean(weights)
-    w <- function(x) {
-        u <- sapply(seq_len(p), function(j) margins_X[[j]](x[j]))
-        Vu <- cbind(V, matrix(rep(u, each = n), n, p))
-        c_YX(Vu) / c_Y(V) * weights
-    }
-    structure(
-        list(
-            copula_method = copula_method,
-            margin_method = margin_method,
-            dots = list(...),
-            y = y,
-            weights = weights,
-            n = n,
-            q = q,
-            p = p,
-            w = w
-        ),
-        class = "eecop"
-    )
+  if (length(weights) == 0) {
+    weights <- rep(1, n)
+  }
+  weights <- weights / mean(weights)
+  w <- function(x) {
+    u <- sapply(seq_len(p), function(j) margins_X[[j]](x[j]))
+    Vu <- cbind(V, matrix(rep(u, each = n), n, p))
+    c_YX(Vu) / c_Y(V) * weights
+  }
+  structure(
+    list(
+      copula_method = copula_method,
+      margin_method = margin_method,
+      dots = list(...),
+      y = y,
+      weights = weights,
+      n = n,
+      q = q,
+      p = p,
+      w = w
+    ),
+    class = "eecop"
+  )
 }
 
 fit_margin <- function(x, method, weights) {
-    switch(method,
-           "normal" = fit_margin_normal(x, weights),
-           "kde"    = fit_margin_kde(x, weights))
+  switch(method,
+    "normal" = fit_margin_normal(x, weights),
+    "kde" = fit_margin_kde(x, weights)
+  )
 }
 
 fit_copula <- function(u, method, weights, ...) {
-    if (NCOL(u) == 1) {
-        return(function(u) rep(1, NROW(u)))
-    }
-    switch(method,
-           "vine"      = fit_copula_vine(u, weights, ...),
-           "gaussian"  = fit_copula_gaussian(u, weights),
-           "kde"       = fit_copula_kde(u, weights),
-           "bernstein" = fit_copula_bernstein(u, weights))
+  if (NCOL(u) == 1) {
+    return(function(u) rep(1, NROW(u)))
+  }
+  switch(method,
+    "vine" = fit_copula_vine(u, weights, ...),
+    "gaussian" = fit_copula_gaussian(u, weights),
+    "kde" = fit_copula_kde(u, weights),
+    "bernstein" = fit_copula_bernstein(u, weights)
+  )
 }
 
 get_psi <- function(type, y) {
-    switch(type,
-           "exp"      = get_psi_expectile(y),
-           "quantile" = get_psi_quantile(y))
+  switch(type,
+    "expectile" = get_psi_expectile(y),
+    "quantile" = get_psi_quantile(y)
+  )
 }
 
 #' Title
@@ -105,25 +108,27 @@ get_psi <- function(type, y) {
 #' @examples
 #'
 #' @importFrom assertthat is.scalar is.string
-predict.eecop <- function(object, x, type = "exp", t = 0.5, ...) {
-    if ((NCOL(x) == 1) & (NROW(x) == object$p)) {
-        x <- t(x)
-    }
-    x <- as.matrix(x)
-    assert_that(
-        ncol(x) == object$p,
-        (is.string(type) & (type %in% c("exp", "quantile"))) | is.function(type),
-        is.scalar(t)
-    )
-    apply(x, 1, predict_one,
-          psi = get_psi(type, object$y),
-          t = t,
-          w = object$w,
-          range = range(y))
+predict.eecop <- function(object, x, type = "expectile", t = 0.5, ...) {
+  if ((NCOL(x) == 1) & (NROW(x) == object$p)) {
+    x <- t(x)
+  }
+  x <- as.matrix(x)
+  assert_that(
+    ncol(x) == object$p,
+    (is.string(type) & (type %in% c("expectile", "quantile"))) |
+      is.function(type),
+    is.scalar(t)
+  )
+  apply(x, 1, predict_one,
+    psi = get_psi(type, object$y),
+    t = t,
+    w = object$w,
+    range = range(y)
+  )
 }
 
 predict_one <- function(x, psi, t, w, range) {
-    Eg <- function(theta) mean(psi(theta, t) * w(t(x)))
-    range <- range + c(-0.25, 0.25) * diff(range)
-    uniroot(Eg, range)$root
+  Eg <- function(theta) mean(psi(theta, t) * w(t(x)))
+  range <- range + c(-0.25, 0.25) * diff(range)
+  uniroot(Eg, range)$root
 }
